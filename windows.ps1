86e012b0ad6b42172c5881293c94049e9c972905
KENKUN-1031 <kencoding1031@gmail.com>
2024-10-23 09:22:14 +0900

    fix: :bug: windows版のchocoが入ってるか確認できるようになった

--- windows.ps1
+++ windows.ps1
@@ -1,3 +1,10 @@
-Remove-Item -Recurse -Force C:\ProgramData\chocolatey
+$choco = ':\ProgramData\chocolatey'
+
+if (Test-Path $choco) {
+  Remove-Item -Recurse -Force $choco
+  Write-Host "Chocolateyが削除されました。"
+} else {
+  Write-Host "Chocolateyは存在しません。削除する必要がありません。"
+}
 
 .¥windows.ps1
